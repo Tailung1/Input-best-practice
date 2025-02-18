6876946d42ed92197fac1f007f22dcb8897e08e4
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-18 22:47:58 +0400

    add input by one of the best oractise

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+
+function App() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); 
+    console.log(userInfo);
+    setUserInfo({ name: "", email: "", password: "" });
+  };
+  const [userInfo, setUserInfo] = useState({name:"",email:"",password:""});
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
+  return (
+    <>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={userInfo.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={userInfo.email}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={userInfo.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
